3b9dbbc3c0712336de040751c55cfb08df9e7407
LeoZhang <1659651473@qq.com>
2025-02-06 11:59:09 +0800

    修复cmd闪退

--- CircleCaculator/main.cpp
+++ CircleCaculator/main.cpp
@@ -155,7 +155,9 @@ int main()
         std::cout << "换算后圆的周长是：" << std::setprecision(9) << ConvertedPerimeter << ConvertLengthUnit << std::endl;
         std::cout << "换算后圆的面积是：" << std::setprecision(9) << ConvertedArea << ConvertLengthUnit << "²" << std::endl;
 
-        std::cout << "程序运行结束" << std::endl; return 0;
+        std::cout << "程序运行结束,回车退出" << std::endl; 
+        getchar();
+        return 0;
 
     }
 
